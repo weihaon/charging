9641de7ccd0f0fa53fa72f80386e2943ee9c5a96
BuptYukino <2023184522@qq.com>
2023-06-05 12:27:53 +0800

    修改了time类
    便于转换为json

--- src/main/java/com/example/application/Class/time.java
+++ src/main/java/com/example/application/Class/time.java
@@ -1,9 +1,5 @@
 package com.example.application.Class;
 
 public class time {
-    public  static class Duration {
-        //大小为8的数组
-
-        public int Enum=-1;
-    }
+    public String time;
 }
